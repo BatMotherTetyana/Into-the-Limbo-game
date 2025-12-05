7889e0dca2d18b138a39b6747abf36e874d4f644
BatMotherTetyana <bat.girl.bts@gmail.com>
2025-12-05 07:55:44 +0200

    settings window base

--- texture_button_5.gd
+++ texture_button_5.gd
@@ -1,11 +1,15 @@
 extends TextureButton
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Подключаем сигнал нажатия кнопки к методу _on_pressed
+	self.pressed.connect(_on_pressed)
 
+# Эта функция вызывается, когда кнопка нажимается.
+func _on_pressed():
+	# Команда, которая завершает работу приложения
+	get_tree().quit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	pass # В кнопке это обычно не нужно
