c37a020ebd52ba8607b1142dcf7c31248d449acc
BatMotherTetyana <bat.girl.bts@gmail.com>
2025-12-17 17:54:45 +0200

    test text

--- scene_1.gd
+++ scene_1.gd
@@ -9,8 +9,8 @@ func _ready():
 	
 	# 2. Список фраз для диалога
 	var my_text = [
-		"Test Text......",
-		"TEST TEXT TEST TEXT TEST TEXT",
+		"Consciousness returns not with a sudden flash, but as a viscous, slow ascent from the depths of a non-existent ocean, where eyelids part to sever the veil of non-being, and the world reveals itself in frozen grisaille, bereft of the breath of life.",
+		"Темные стебли травы, подобные замерзшим струнам, едва касаются босых ступней — прикосновение, полное холодной, отстраненной нежности, пока ветер, лишенный направления, скользит по коже, не освежая, а будто слизывая остатки тепла и унося с собой осколки прошлого.",
 		"LIAFGULGufLGUIlifyFYLAUyflwlFYLwuWlAFIWYiwfglaGUFIWF^A&@*7q8429w8q964q28????///////",
 		"qwertyuiop[]asdfghjkl;'zxcvbnm,./"
 	]
